edac941db2e0034734cae5067029eb6119748857
Miguel <Migue@LAPTOP-91M8P1HF>
2025-08-27 16:49:41 -0600

    Resolucion de los conversores usando matricez

--- Conversor (2)/Conversor/Conversor4.0/Conversor4.0/Form1.Designer.cs
+++ Conversor (2)/Conversor/Conversor4.0/Conversor4.0/Form1.Designer.cs
@@ -28,11 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.cmbTipo = new System.Windows.Forms.ComboBox();
-            this.cmbOrigen = new System.Windows.Forms.ComboBox();
-            this.cmbDestino = new System.Windows.Forms.ComboBox();
-            this.txtValor = new System.Windows.Forms.TextBox();
-            this.lblResultado = new System.Windows.Forms.Label();
+            this.cboTipoConversor = new System.Windows.Forms.ComboBox();
+            this.cboDeConversor = new System.Windows.Forms.ComboBox();
+            this.cboAConversor = new System.Windows.Forms.ComboBox();
+            this.txtCantidadConversor = new System.Windows.Forms.TextBox();
+            this.lblRespuestaConversor = new System.Windows.Forms.Label();
             this.btnConvertir = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -42,56 +42,64 @@
             this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // cmbTipo
+            // cboTipoConversor
             // 
-            this.cmbTipo.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.cmbTipo.FormattingEnabled = true;
-            this.cmbTipo.Location = new System.Drawing.Point(245, 91);
-            this.cmbTipo.Name = "cmbTipo";
-            this.cmbTipo.Size = new System.Drawing.Size(161, 21);
-            this.cmbTipo.TabIndex = 0;
+            this.cboTipoConversor.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.cboTipoConversor.FormattingEnabled = true;
+            this.cboTipoConversor.Items.AddRange(new object[] {
+            "Moneda",
+            "Longitud",
+            "Masa",
+            "Volumen",
+            "Almacenamiento",
+            "Tiempo"});
+            this.cboTipoConversor.Location = new System.Drawing.Point(96, 85);
+            this.cboTipoConversor.Name = "cboTipoConversor";
+            this.cboTipoConversor.Size = new System.Drawing.Size(161, 21);
+            this.cboTipoConversor.TabIndex = 0;
+            this.cboTipoConversor.SelectedIndexChanged += new System.EventHandler(this.cboTipoConversor_SelectedIndexChanged);
             // 
-            // cmbOrigen
+            // cboDeConversor
             // 
-            this.cmbOrigen.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.cmbOrigen.FormattingEnabled = true;
-            this.cmbOrigen.Location = new System.Drawing.Point(245, 165);
-            this.cmbOrigen.Name = "cmbOrigen";
-            this.cmbOrigen.Size = new System.Drawing.Size(161, 21);
-            this.cmbOrigen.TabIndex = 1;
+            this.cboDeConversor.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.cboDeConversor.FormattingEnabled = true;
+            this.cboDeConversor.Location = new System.Drawing.Point(96, 157);
+            this.cboDeConversor.Name = "cboDeConversor";
+            this.cboDeConversor.Size = new System.Drawing.Size(161, 21);
+            this.cboDeConversor.TabIndex = 1;
             // 
-            // cmbDestino
+            // cboAConversor
             // 
-            this.cmbDestino.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.cmbDestino.FormattingEnabled = true;
-            this.cmbDestino.Location = new System.Drawing.Point(245, 192);
-            this.cmbDestino.Name = "cmbDestino";
-            this.cmbDestino.Size = new System.Drawing.Size(161, 21);
-            this.cmbDestino.TabIndex = 2;
+            this.cboAConversor.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.cboAConversor.FormattingEnabled = true;
+            this.cboAConversor.Location = new System.Drawing.Point(96, 187);
+            this.cboAConversor.Name = "cboAConversor";
+            this.cboAConversor.Size = new System.Drawing.Size(161, 21);
+            this.cboAConversor.TabIndex = 2;
             // 
-            // txtValor
+            // txtCantidadConversor
             // 
-            this.txtValor.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.txtValor.Location = new System.Drawing.Point(245, 247);
-            this.txtValor.Name = "txtValor";
-            this.txtValor.Size = new System.Drawing.Size(161, 20);
-            this.txtValor.TabIndex = 3;
+            this.txtCantidadConversor.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtCantidadConversor.Location = new System.Drawing.Point(96, 272);
+            this.txtCantidadConversor.Name = "txtCantidadConversor";
+            this.txtCantidadConversor.Size = new System.Drawing.Size(161, 20);
+            this.txtCantidadConversor.TabIndex = 3;
             // 
-            // lblResultado
+            // lblRespuestaConversor
             // 
-            this.lblResultado.AutoSize = true;
-            this.lblResultado.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblResultado.Location = new System.Drawing.Point(70, 369);
-            this.lblResultado.Name = "lblResultado";
-            this.lblResultado.Size = new System.Drawing.Size(148, 22);
-            this.lblResultado.TabIndex = 4;
-            this.lblResultado.Text = "La respuesta es:";
+            this.lblRespuestaConversor.AutoSize = true;
+            this.lblRespuestaConversor.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRespuestaConversor.Location = new System.Drawing.Point(29, 325);
+            this.lblRespuestaConversor.Name = "lblRespuestaConversor";
+            this.lblRespuestaConversor.Size = new System.Drawing.Size(148, 22);
+            this.lblRespuestaConversor.TabIndex = 4;
+            this.lblRespuestaConversor.Text = "La respuesta es:";
             // 
             // btnConvertir
             // 
             this.btnConvertir.BackColor = System.Drawing.Color.Silver;
             this.btnConvertir.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnConvertir.Location = new System.Drawing.Point(311, 306);
+            this.btnConvertir.Location = new System.Drawing.Point(127, 217);
             this.btnConvertir.Name = "btnConvertir";
             this.btnConvertir.Size = new System.Drawing.Size(95, 37);
             this.btnConvertir.TabIndex = 5;
@@ -103,7 +111,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Cambria", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(206, 33);
+            this.label1.Location = new System.Drawing.Point(73, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(149, 28);
             this.label1.TabIndex = 6;
@@ -113,7 +121,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(158, 90);
+            this.label2.Location = new System.Drawing.Point(9, 84);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(81, 19);
             this.label2.TabIndex = 7;
@@ -123,7 +131,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(158, 136);
+            this.label3.Location = new System.Drawing.Point(135, 121);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(81, 19);
             this.label3.TabIndex = 8;
@@ -133,7 +141,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(216, 194);
+            this.label4.Location = new System.Drawing.Point(67, 186);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(23, 19);
             this.label4.TabIndex = 9;
@@ -143,7 +151,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(187, 247);
+            this.label5.Location = new System.Drawing.Point(38, 272);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(52, 19);
             this.label5.TabIndex = 10;
@@ -153,7 +161,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(207, 167);
+            this.label6.Location = new System.Drawing.Point(58, 159);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(32, 19);
             this.label6.TabIndex = 11;
@@ -164,7 +172,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gainsboro;
-            this.ClientSize = new System.Drawing.Size(592, 436);
+            this.ClientSize = new System.Drawing.Size(300, 362);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -172,14 +180,13 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnConvertir);
-            this.Controls.Add(this.lblResultado);
-            this.Controls.Add(this.txtValor);
-            this.Controls.Add(this.cmbDestino);
-            this.Controls.Add(this.cmbOrigen);
-            this.Controls.Add(this.cmbTipo);
+            this.Controls.Add(this.lblRespuestaConversor);
+            this.Controls.Add(this.txtCantidadConversor);
+            this.Controls.Add(this.cboAConversor);
+            this.Controls.Add(this.cboDeConversor);
+            this.Controls.Add(this.cboTipoConversor);
             this.Name = "Form1";
             this.Text = "Conversor";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -187,11 +194,11 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox cmbTipo;
-        private System.Windows.Forms.ComboBox cmbOrigen;
-        private System.Windows.Forms.ComboBox cmbDestino;
-        private System.Windows.Forms.TextBox txtValor;
-        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.ComboBox cboTipoConversor;
+        private System.Windows.Forms.ComboBox cboDeConversor;
+        private System.Windows.Forms.ComboBox cboAConversor;
+        private System.Windows.Forms.TextBox txtCantidadConversor;
+        private System.Windows.Forms.Label lblRespuestaConversor;
         private System.Windows.Forms.Button btnConvertir;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
