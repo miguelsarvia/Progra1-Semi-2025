012a457e78f15a0f6c3dbce8738c2840473a2352
Miguel <Migue@LAPTOP-91M8P1HF>
2025-08-28 17:01:44 -0600

    Calculos de la media aritmetica, desviacion tipica y la media armonica.

--- Conversor (2)/Conversor/Conversor4.0/Conversor4.0/Form1.Designer.cs
+++ Conversor (2)/Conversor/Conversor4.0/Conversor4.0/Form1.Designer.cs
@@ -28,21 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblSerieNumeros = new System.Windows.Forms.Label();
+            this.txtSerie = new System.Windows.Forms.TextBox();
+            this.lblMedia = new System.Windows.Forms.Label();
+            this.lblTipica = new System.Windows.Forms.Label();
+            this.lblArmonica = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(125, 188);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(125, 49);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblSerieNumeros
+            // 
+            this.lblSerieNumeros.AutoSize = true;
+            this.lblSerieNumeros.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSerieNumeros.Location = new System.Drawing.Point(33, 62);
+            this.lblSerieNumeros.Name = "lblSerieNumeros";
+            this.lblSerieNumeros.Size = new System.Drawing.Size(65, 24);
+            this.lblSerieNumeros.TabIndex = 1;
+            this.lblSerieNumeros.Text = "Serie:";
+            // 
+            // txtSerie
+            // 
+            this.txtSerie.Location = new System.Drawing.Point(125, 66);
+            this.txtSerie.Name = "txtSerie";
+            this.txtSerie.Size = new System.Drawing.Size(138, 20);
+            this.txtSerie.TabIndex = 2;
+            // 
+            // lblMedia
+            // 
+            this.lblMedia.AutoSize = true;
+            this.lblMedia.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMedia.Location = new System.Drawing.Point(33, 96);
+            this.lblMedia.Name = "lblMedia";
+            this.lblMedia.Size = new System.Drawing.Size(73, 24);
+            this.lblMedia.TabIndex = 3;
+            this.lblMedia.Text = "Media:";
+            // 
+            // lblTipica
+            // 
+            this.lblTipica.AutoSize = true;
+            this.lblTipica.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipica.Location = new System.Drawing.Point(33, 124);
+            this.lblTipica.Name = "lblTipica";
+            this.lblTipica.Size = new System.Drawing.Size(73, 24);
+            this.lblTipica.TabIndex = 4;
+            this.lblTipica.Text = "Tipica:";
+            // 
+            // lblArmonica
+            // 
+            this.lblArmonica.AutoSize = true;
+            this.lblArmonica.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArmonica.Location = new System.Drawing.Point(33, 152);
+            this.lblArmonica.Name = "lblArmonica";
+            this.lblArmonica.Size = new System.Drawing.Size(105, 24);
+            this.lblArmonica.TabIndex = 5;
+            this.lblArmonica.Text = "Armonica:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gainsboro;
-            this.ClientSize = new System.Drawing.Size(566, 369);
+            this.ClientSize = new System.Drawing.Size(371, 274);
+            this.Controls.Add(this.lblArmonica);
+            this.Controls.Add(this.lblTipica);
+            this.Controls.Add(this.lblMedia);
+            this.Controls.Add(this.txtSerie);
+            this.Controls.Add(this.lblSerieNumeros);
+            this.Controls.Add(this.btnCalcular);
             this.Name = "Form1";
             this.Text = "Estadistica";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblSerieNumeros;
+        private System.Windows.Forms.TextBox txtSerie;
+        private System.Windows.Forms.Label lblMedia;
+        private System.Windows.Forms.Label lblTipica;
+        private System.Windows.Forms.Label lblArmonica;
     }
 }
 
