10eed64de9274f7b406d21ca6d1129b6c038f187
Miguel <Migue@LAPTOP-91M8P1HF>
2025-11-14 16:53:48 -0600

    programacion del formulario de periodos.

---
+++ webappacademica/webappacademica/Models/Periodo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webappacademica.Models
+{
+    public class Periodo
+    {
+        [Key]
+        public int idPeriodo { get; set; }
+        public string fecha { get; set; }
+        public string periodo { get; set; }
+    }
+}
