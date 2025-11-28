c27c062d0e0177595dda0ed91820f2cef46b9e73
Miguel <Migue@LAPTOP-91M8P1HF>
2025-11-28 10:04:49 -0600

    iniciamos el CRUD de matriculas.

--- webappacademica/webappacademica/Models/Periodo.cs
+++ webappacademica/webappacademica/Models/Periodo.cs
@@ -12,5 +12,7 @@ namespace webappacademica.Models
         public int idPeriodo { get; set; }
         public DateTime fecha { get; set; }
         public string periodo { get; set; }
+        //Relacion con la tabla matriculas
+        public ICollection<Matricula> Matriculas { get; set; }
     }
 }
--- webappacademica/webappacademica/obj/Debug/net8.0/webappacademica.AssemblyInfo.cs
+++ webappacademica/webappacademica/obj/Debug/net8.0/webappacademica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webappacademica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10eed64de9274f7b406d21ca6d1129b6c038f187")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+450ec034a9b8e0d473fea74d4ff8de3e1f37e8cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("webappacademica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webappacademica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
