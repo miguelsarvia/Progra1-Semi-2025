4ffe458ec5b9e5b2fe418b6dffb476e6d479d7d4
Miguel <Migue@LAPTOP-91M8P1HF>
2025-12-04 21:12:32 -0600

    Proyecto Final.

---
+++ README.md
@@ -0,0 +1,25 @@
+# Progra1-Semi-2025
+Códigos, ejemplos y practica de la materia Programación Computacional 1
+
+# Proyecto Final
+## Tecnologias a utilizar
+* Visual Studio 2026
+* C#
+* SQL Server Express -u otros
+* ASP.NET
+* Entity Framwork
+* HTML + CSS + JS
+* BootStrap u otros
+* API REST FULL
+* Git - Github, u otros
+* Etc.
+
+## Requisitos minimos del proyecto final
+* Equipos de trabajo
+* Bases de datos
+* 5 Formularios basicos (complejidad basica o una tabla)
+* 3 Formularios intermedios (complejidad media o con 2 o 3 tablas)
+* 3 Formularios Avanzados (Alta complejidad o 3 o mas tablas anidadas)
+* 10 reportes (salidad de informacion o presentacion de informacion de usuario)
+* Menus y acceso rapido, barras de herramientas, cuadros de dialogo
+* Creatividad e innovacion.
---
+++ miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace miPrimerProyectoCsharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSerie = new System.Windows.Forms.Label();
+            this.lblSuma = new System.Windows.Forms.Label();
+            this.txtSerie = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSerie
+            // 
+            this.lblSerie.AutoSize = true;
+            this.lblSerie.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSerie.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.lblSerie.Location = new System.Drawing.Point(72, 20);
+            this.lblSerie.Name = "lblSerie";
+            this.lblSerie.Size = new System.Drawing.Size(64, 20);
+            this.lblSerie.TabIndex = 0;
+            this.lblSerie.Text = "SERIE";
+            // 
+            // lblSuma
+            // 
+            this.lblSuma.AutoSize = true;
+            this.lblSuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSuma.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.lblSuma.Location = new System.Drawing.Point(72, 61);
+            this.lblSuma.Name = "lblSuma";
+            this.lblSuma.Size = new System.Drawing.Size(80, 20);
+            this.lblSuma.TabIndex = 1;
+            this.lblSuma.Text = "SUMA: ?";
+            this.lblSuma.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtSerie
+            // 
+            this.txtSerie.Location = new System.Drawing.Point(157, 20);
+            this.txtSerie.Name = "txtSerie";
+            this.txtSerie.Size = new System.Drawing.Size(100, 20);
+            this.txtSerie.TabIndex = 2;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.btnCalcular.Location = new System.Drawing.Point(118, 107);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(86, 31);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 150);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtSerie);
+            this.Controls.Add(this.lblSuma);
+            this.Controls.Add(this.lblSerie);
+            this.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSerie;
+        private System.Windows.Forms.Label lblSuma;
+        private System.Windows.Forms.TextBox txtSerie;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
